6edec049d6bb1d169e6399f43deb52da54ec32dc
apryamostanov <anton.pryamostanov@gmail.com>
2019-04-14 13:48:10 +0400

    Remove unused statuses

--- pigeon-lib/src/main/groovy/io/infinite/pigeon/other/MessageStatuses.groovy
+++ pigeon-lib/src/main/groovy/io/infinite/pigeon/other/MessageStatuses.groovy
@@ -6,15 +6,12 @@ enum MessageStatuses {
     NEW2("NEW"),
     DELIVERED("delivered"),
     FAILED_NO_CONNECTION("no_connection"),
-    FAILED_INVALID_RESPONSE("invalid_response"),
-    FAILED_INVALID_REQUEST("invalid_request"),
     FAILED_RESPONSE("failed_response"),
     EXCEPTION("error"),
     WAITING("waiting"),
     DUPLICATE("duplicate"),
     RENEWED("renewed"),
     SENDING("sending"),
-    OBSOLETE("obsolete"),
     SPLIT("split")
 
     private final String messageStatus
