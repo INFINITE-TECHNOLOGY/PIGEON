2db443a48c5ef55eefa7f3e3849c1c1ddc1744be
apryamostanov <anton.pryamostanov@gmail.com>
2019-09-06 01:28:55 +0400

    Progress

--- pigeon-lib/src/main/groovy/io/infinite/pigeon/springdatarest/configurations/security/JwtTokenAuthenticationFilter.groovy
+++ pigeon-lib/src/main/groovy/io/infinite/pigeon/springdatarest/configurations/security/JwtTokenAuthenticationFilter.groovy
@@ -1,11 +1,14 @@
 package io.infinite.pigeon.springdatarest.configurations.security
 
-
+import com.fasterxml.jackson.databind.ObjectMapper
 import groovy.util.logging.Slf4j
-import io.infinite.ascend.granting.model.Authorization
-import io.infinite.ascend.validation.AuthorizationValidator
+import io.infinite.ascend.other.AscendException
+import io.infinite.ascend.validation.model.AscendHttpRequest
 import io.infinite.blackbox.BlackBox
 import io.infinite.carburetor.CarburetorLevel
+import io.infinite.pigeon.http.HttpRequest
+import io.infinite.pigeon.http.HttpResponse
+import io.infinite.pigeon.http.SenderDefaultHttps
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
 import org.springframework.web.filter.OncePerRequestFilter
@@ -14,7 +17,6 @@ import javax.servlet.FilterChain
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-
 /**
  * https://github.com/OmarElGabry/microservices-spring-boot/blob/master/spring-eureka-zuul/src/main/java/com/eureka/zuul/security/JwtTokenAuthenticationFilter.java
  */
@@ -23,21 +25,43 @@ import javax.servlet.http.HttpServletResponse
 @BlackBox
 class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
 
-    AuthorizationValidator authorizationValidator
-
     @Override
     @BlackBox(level = CarburetorLevel.METHOD)
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
         try {
+            ObjectMapper objectMapper = new ObjectMapper()
             String authorizationHeader = request.getHeader("Authorization")
             if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
                 chain.doFilter(request, response)
                 return
             }
-            Authorization authorization = authorizationValidator.validateAuthorizationHeader(authorizationHeader, request)
+            AscendHttpRequest ascendHttpRequest = new AscendHttpRequest(
+                    authorizationHeader: authorizationHeader,
+                    incomingUrl: request.requestURL
+                            .append('?')
+                            .append(request.getQueryString())
+                            .toString(),
+                    method: request.method,
+                    body: null as String
+            )
+            HttpRequest ascendRequest = new HttpRequest(
+                    url: "https://ascend-gfs.herokuapp.com/ascend/validation",
+                    headers: ["content-type": "application/json"],
+                    method: "POST",
+                    body: objectMapper.writeValueAsString(ascendHttpRequest)
+            )
+            HttpResponse ascendResponse = new HttpResponse()
+            new SenderDefaultHttps().sendHttpMessage(ascendRequest, ascendResponse)
+            if (ascendResponse.status != 200) {
+                throw new AscendException("Failed Ascend HTTP status")
+            }
+            AscendHttpRequest ascendHttpResponse = objectMapper.readValue(ascendResponse.body, AscendHttpRequest.class)
+            if (ascendResponse.status != 200) {
+                throw new AscendException("Unauthorized.")
+            }
             PreAuthenticatedAuthenticationToken preAuthenticatedAuthenticationToken =
-                    new PreAuthenticatedAuthenticationToken(authorization.identity, authorization.identity?.authentications)
+                    new PreAuthenticatedAuthenticationToken(ascendHttpResponse.authorization.identity, ascendHttpResponse.authorization.identity?.authentications)
             preAuthenticatedAuthenticationToken.setAuthenticated(true)
             SecurityContextHolder.getContext().setAuthentication(preAuthenticatedAuthenticationToken)
         }
--- pigeon-lib/src/main/groovy/io/infinite/pigeon/springdatarest/configurations/security/SecurityTokenConfig.groovy
+++ pigeon-lib/src/main/groovy/io/infinite/pigeon/springdatarest/configurations/security/SecurityTokenConfig.groovy
@@ -1,19 +1,13 @@
 package io.infinite.pigeon.springdatarest.configurations.security
 
 import groovy.util.logging.Slf4j
-import io.infinite.ascend.common.JwtManager
-import io.infinite.ascend.validation.AuthorizationValidator
 import io.infinite.blackbox.BlackBox
-import io.infinite.pigeon.springdatarest.repositories.AscendUsageRepository
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter
 
-import javax.annotation.PostConstruct
 import javax.servlet.http.HttpServletResponse
 /**
  * https://github.com/OmarElGabry/microservices-spring-boot/blob/master/spring-eureka-zuul/src/main/java/com/eureka/zuul/security/SecurityTokenConfig.java
@@ -22,31 +16,10 @@ import javax.servlet.http.HttpServletResponse
 @Slf4j
 class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
 
-    AuthorizationValidator authorizationValidator
-
-    @Autowired
-    AscendUsageRepository ascendUsageRepository
-
-    @Value('${ascendPublicKeyUrl}')
-    String ascendPublicKeyUrl
-
-    @Value('${ascendSenderClassName}')
-    String ascendSenderClassName
-
-    @PostConstruct
-    void init() {
-        JwtManager jwtManager = new JwtManager()
-        jwtManager.setJwtAccessKeyPublic(jwtManager.loadPublicKeyFromEnv("TRUSTED_ASCEND_PUBLIC_KEY"))
-        authorizationValidator = new AuthorizationValidator(
-                jwtManager: jwtManager,
-                usageRepository: ascendUsageRepository)
-    }
-
     @Override
     @BlackBox
     protected void configure(HttpSecurity http) throws Exception {
-        JwtTokenAuthenticationFilter jwtTokenAuthenticationFilter = new JwtTokenAuthenticationFilter(
-                authorizationValidator: authorizationValidator)
+        JwtTokenAuthenticationFilter jwtTokenAuthenticationFilter = new JwtTokenAuthenticationFilter()
         http
                 .csrf().disable()
         // make sure we use stateless session session won't be used to store user's state.
